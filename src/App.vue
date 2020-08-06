30017d7b09f041f302b3c3cef7331d44fc9c222f
protonate <nathan@springvilleprojects.com>
2020-08-06 15:41:26 -0700

    toolchain proof changes

--- src/App.vue
+++ src/App.vue
@@ -8,18 +8,6 @@
   </div>
 </template>
 
-<script>
-import NavBar from "./components/NavBar.vue";
-import Footer from "./components/Footer.vue";
-
-export default {
-  components: {
-    NavBar,
-    Footer
-  }
-};
-</script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss">
 @import "./assets/styles/main.scss";
